7716ce2cd93ca6a493e7cf3b1266eac4fc4f5a05
Vinicius Mussato <vinicius.mussato@gmail.com>
2018-01-26 13:50:15 -0200

    Added start script and basic server structure

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('This is working');
+})
+
+app.listen(3000, () => {
+	console.log('App is running on port 3000');
+})
+
+/*
+/ --> res = this is working
+/signin --> POST success/fail
+/register --> POST = user
+/profile/:iserId --> GET = user
+/image --> PUT --> user
+
+*/
