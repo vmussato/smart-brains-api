ca4264de2fd2d503a84c6f3958670617feb516db
Vinicius Mussato <vinicius.mussato@gmail.com>
2018-01-27 21:47:25 -0200

    Added knex and postgres + register is now integrated with db

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'vinicius',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 
 const app = express();
@@ -58,13 +69,11 @@ app.post('/signin' , (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	database.users.push({
-		id: '125',
-		name: name,
+	db('users').insert({
 		email: email,
-		entries: 0,
+		name: name,
 		joined: new Date()
-	})
+	}).then(console.log)
 	res.json(database.users[database.users.length-1])
 })
 
