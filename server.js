85738847f6fc40995a862d1b9f9ee04e0724eb4c
Vinicius Mussato <vinicius.mussato@gmail.com>
2018-01-28 15:36:17 -0200

    change root response

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('is is working') })
 app.post('/signin' , signin.handleSignIn(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
