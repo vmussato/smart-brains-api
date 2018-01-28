6db1d7b2a0de650047c78db82e8babf5b764151b
Vinicius Mussato <vinicius.mussato@gmail.com>
2018-01-28 16:44:18 -0200

    Update database options

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-vertical-26463',
-    user : 'vinicius',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
+    
   }
 });
 
