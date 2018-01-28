3a2044d53e20c873fec6138f3de5976bb0aa6524
Vinicius Mussato <vinicius.mussato@gmail.com>
2018-01-28 11:49:32 -0200

    Added clarifai to the back-end and made some validations

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,16 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'e3731871a5824f5581a3d9a78dba1f47'
+});
+const handleApiCall = (req, res) => {
+app.models
+	.predict( Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json('unable to work with API'))
+}
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
 	db('users').where('id', '=', id)
@@ -10,5 +23,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-	handleImage: handleImage
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
 }
