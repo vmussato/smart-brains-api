ab849634b13b0a8216e7c3759b390d3f723b7914
Vmussato <vinicius.mussato@gmail.com>
2018-01-30 23:45:21 -0200

    Test

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT, () => {
-	console.log(`App is running on port ${process.env.PORT} `);
+	console.log(`App is running on port 3000`);
 })
