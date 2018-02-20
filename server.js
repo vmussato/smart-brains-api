6991d7c5f591daf4b395ca7f56a617539314535d
Vmussato <vinicius.mussato@gmail.com>
2018-02-20 17:44:24 -0300

    Testing some shit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
+//making a test
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
